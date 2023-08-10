6f85eb0d80deadd0491ad93eed9a916171394cf9
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2023-08-10 20:22:51 -0300

    ✨: create emoji grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,42 @@
 
 	type State = 'start' | 'playing' | 'paused' | 'won' | 'lost';
 	let state: State = 'start';
+
+	let size = 20;
+	let grid = createGrid() ?? [];
+	let maxMatches = grid.length / 2;
+	let selected: number[] = [];
+	let matches: string[] = [];
+
+	function createGrid() {
+		const cards = new Set<string>();
+		const maxSize = size / 2;
+
+		while (cards.size < maxSize) {
+			const randomIndex = Math.floor(Math.random() * emoji.length);
+
+			cards.add(emoji[randomIndex]);
+		}
+		return shuffle([...cards, ...cards]);
+	}
+	function shuffle<T>(items: T[]) {
+		return items.sort(() => Math.random() - 0.5);
+	}
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+{#if state === 'start'}
+	<h1>Matching game</h1>
+	<button on:click={() => (state = 'playing')}>Play</button>
+{/if}
+
+{#if state === 'playing'}
+	<div class="cards">
+		{#each grid as card, index}
+			<button>
+				<div class="card">
+					{card}
+				</div>
+			</button>
+		{/each}
+	</div>
+{/if}
