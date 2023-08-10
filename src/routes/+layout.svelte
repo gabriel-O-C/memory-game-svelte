c52cec34e6f6a7048e3e7e5c4ceea70382b08923
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2023-08-10 19:56:13 -0300

    🎉: init projetc

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '../global.css'
+</script>
+
+<svelte:head>
+	<title>Matching Game</title>
+</svelte:head>
+
+<slot />
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { emoji } from './emoji';
+
+	type State = 'start' | 'playing' | 'paused' | 'won' | 'lost';
+	let state: State = 'start';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
